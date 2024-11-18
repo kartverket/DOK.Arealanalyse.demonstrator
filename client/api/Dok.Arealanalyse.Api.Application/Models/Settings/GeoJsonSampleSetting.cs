f03edc9d497a55de1932b738d444cf1377d8bf68
Tor Anders Gustavsen <tor.anders@arkitektum.no>
2024-11-18 11:41:01 +0100

    La til flere eksempelfiler

--- client/api/Dok.Arealanalyse.Api.Application/Models/Settings/GeoJsonSampleSetting.cs
+++ client/api/Dok.Arealanalyse.Api.Application/Models/Settings/GeoJsonSampleSetting.cs
@@ -2,7 +2,6 @@
 
 public class GeoJsonSampleSetting
 {
-    public static readonly string SectionName = "GeoJsonSamples";
     public string Name { get; set; }
     public string Description { get; set; }
     public string FileName { get; set; }
--- client/api/Dok.Arealanalyse.Api.Application/Services/GeoJsonSampleService.cs
+++ client/api/Dok.Arealanalyse.Api.Application/Services/GeoJsonSampleService.cs
@@ -2,21 +2,19 @@
 using Dok.Arealanalyse.Api.Application.Models.Settings;
 using Microsoft.Extensions.Configuration;
 using System.Reflection;
+using System.Text.Json;
 using System.Text.Json.Nodes;
 
 namespace Dok.Arealanalyse.Api.Application.Services;
 
-public class GeoJsonSampleService(
-    IConfiguration configuration) : IGeoJsonSampleService
+public class GeoJsonSampleService : IGeoJsonSampleService
 {
     private static readonly string _jsonFilesPath = GetGeoJsonFilesPath();
+    private static readonly JsonSerializerOptions _jsonSerializerOptions = new() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
 
     public async Task<List<GeoJsonSample>> GetSamplesAsync()
     {
-        var settings = configuration
-            .GetSection(GeoJsonSampleSetting.SectionName)
-            .Get<List<GeoJsonSampleSetting>>();
-
+        var settings = await ReadJsonFileAsync<List<GeoJsonSampleSetting>>("config.json");
         var samples = new List<GeoJsonSample>();
 
         foreach (var setting in settings)
@@ -26,19 +24,20 @@ public class GeoJsonSampleService(
                 Name = setting.Name,
                 Description = setting.Description,
                 FileName = setting.FileName,
-                GeoJson = await ReadGeoJsonFileAsync(setting.FileName)
+                GeoJson = await ReadJsonFileAsync<JsonNode>(setting.FileName)
             });
         }
 
         return [.. samples.OrderBy(sample => sample.Name)];
     }
 
-    private static async Task<JsonNode> ReadGeoJsonFileAsync(string fileName)
+    private static async Task<T> ReadJsonFileAsync<T>(string fileName)
+        where T : class
     {
         var path = Path.Combine(_jsonFilesPath, fileName);
         var json = await File.ReadAllTextAsync(path);
 
-        return JsonNode.Parse(json);
+        return JsonSerializer.Deserialize<T>(json, _jsonSerializerOptions);
     }
 
     private static string GetGeoJsonFilesPath()
